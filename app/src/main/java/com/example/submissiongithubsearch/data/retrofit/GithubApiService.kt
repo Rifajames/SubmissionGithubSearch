6e4ed05237a843b8fb4baf4fc8af2a5ed3762af2
muhamadrifajames <muhamad.james@graveltechnology.com>
2023-11-03 15:04:21 +0700

    new commit

--- app/src/main/java/com/example/submissiongithubsearch/data/retrofit/GithubApiService.kt
+++ app/src/main/java/com/example/submissiongithubsearch/data/retrofit/GithubApiService.kt
@@ -11,28 +11,28 @@ import retrofit2.http.Query
 
 interface GithubApiService {
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getUser(
         @Query("q")
         query: String
     ): Call<ResponseGithub>
 
     @GET("users/{username}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getGithubDetail(
         @Path("username")
         username: String
     ): Call<DetailResponse>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getFollower(
         @Path("username")
         username: String
     ): Call<List<ResponseFollow>>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
    fun getFollowing(
         @Path("username")
         username: String
